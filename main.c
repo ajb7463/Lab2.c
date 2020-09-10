66bf395170952cf7fbf65cc49241329f99ed311f
Alex Barr <ajb7463@psu.edu>
2020-09-10 20:12:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+// Author: Alexander Barr ajb7463@psu.edu
+// Collaborator: Raymond Chen rpc5459@psu.edu
+// Collaborator: Stanley Wang szw5739@psu.edu
+// Collaborator: Chao-Yang Fang cbf5338@psu.edu
+// Section: 10
+// Breakout: 14
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *grade = readline("Enter your CMPSC 131 grade: ");
+  double x = atof(grade);
+  char *letter = " ";
+  if (x >= 93.0) {
+    char *letter = "A";
+  }
+  else if(x >= 90.0 && x < 93.0) {
+    char *letter = "A-";
+  }
+  else if(x >= 87.0 && x < 90.0) {
+    char *letter = "B+";
+  }
+  else if(x >= 83.0 && x < 87.0) {
+    char *letter = "B";
+  }
+  else if(x >= 80.0 && x < 83.0) {
+    char *letter = "B-";
+  }
+  else if(x >= 77.0 && x < 80.0) {
+    char *letter = "C+";
+  }
+  else if(x >= 70 && x < 77.0) {
+    char *letter = "C";
+  }
+  else if(x >= 60.0 && x < 70.0) {
+    char *letter = "D";
+  }
+  else if(x < 60.0) {
+    char *letter = "F";
+  }
+  printf("Your letter grade for CMPSC 131 is %s.\n", letter);
+  return 0;
+}
